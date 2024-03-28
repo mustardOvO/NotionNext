7166e362eb168e8a634173ea7fa6dc935fa2abc4
mustardovo <mustardovo@gmail.com>
2024-03-28 23:06:31 +0800

    1

--- themes/00mine/components/BlogListMasonryScroll.js
+++ themes/00mine/components/BlogListMasonryScroll.js
@@ -7,27 +7,34 @@ import imagesLoaded from 'imagesloaded';
 import BlogPost from './BlogPost'
 import dynamic from 'next/dynamic'
 import NotionPage from '@/components/NotionPage'
+import { useHexoGlobal } from '..' //删掉
 
 
 
 const BlogListScroll = props => {
-  const { posts, allPages} = props
+  const { posts, notice, allPages} = props
   const { locale } = useGlobal()
-  const [postnum, updatepostnum] = useState(6)
+  const [postnum, updatepostnum] = useState(9)
   // let postnum=parseInt(siteConfig('POSTS_PER_PAGE'))
+
+  //删掉这段————————
+  // const { showModal, setShowModal, modalContent, setModalContent } = useHexoGlobal()
+  // setShowModal(true)
+  // setModalContent(posts[2])
+  //————————————————
 
   let hasMore = false
   const postsToShow = posts
-    // ? Object.assign(posts).slice(0, postnum<5? 5:postnum)
-    // : []
+    ? Object.assign(posts).slice(0, postnum<5? 5:postnum)
+    : []
 
   if (posts) {
     const totalCount = posts.length
     hasMore = postnum < totalCount
   }
   const handleGetMore = () => {
-    if (!hasMore) return
-    updatepostnum(postnum + 3)
+    //if (!hasMore) return
+    updatepostnum(postnum + 5)
   }
 
   const targetRef = useRef(null)
@@ -44,7 +51,7 @@ const BlogListScroll = props => {
                 columnWidth: '.grid-sizer',
                 gutter: 0,
                 percentPosition: true,
-                // stagger: 30,
+                stagger: 30,
                 // transitionDuration: '0.8s'
                 // initLayout: false,
                 // horizontalOrder: true,
@@ -67,7 +74,7 @@ const BlogListScroll = props => {
     const scrollS = window.scrollY + window.outerHeight
     const clientHeight = targetRef ? (targetRef.current ? (targetRef.current.clientHeight) : 0) : 0
     
-    if (scrollS > clientHeight+200) {
+    if (scrollS > clientHeight+100) {
       handleGetMore()
       imagesLoaded( gridRef.current ).on( 'progress', function() {
         // layout Masonry after each image loads
@@ -95,7 +102,7 @@ const BlogListScroll = props => {
       <div ref={targetRef}>
         
 
-        <div className="grid pb-32 w-full mx-auto max-w-[1800px] "  ref={gridRef}>
+        <div className="grid pb-32 w-full mx-auto max-w-[1800px]"  ref={gridRef}>
           <div className="grid-sizer"></div>
               {postsToShow.map((post, index) => (
                 <BlogPost index={index} key={post.id} className="grid-item" post={post} {...props} />
