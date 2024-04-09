cdc425466c6fa900822773fcbd1e9b72c1bc2bdf
mustardOvO <mustardovo@gmail.com>
2024-04-09 18:37:45 +0800

    1

--- pages/new11.js
+++ pages/new11.js
@@ -9,10 +9,10 @@ import { getGlobalData } from '@/lib/notion/getNotionData'
  * @returns
  */
 const new1 = props => {
-    // 根据页面路径加载不同Layout文件
-    //   const Layout = getLayoutByTheme({ theme: siteConfig('THEME'), router: useRouter() })
-    //   return <Layout {...props} />
-    return <div className="w-full h-20 bg-red-400">111</div>
+  // 根据页面路径加载不同Layout文件
+  //   const Layout = getLayoutByTheme({ theme: siteConfig('THEME'), router: useRouter() })
+  //   return <Layout {...props} />
+  return <div className="w-full h-20 bg-red-400">111</div>
 }
 
 export async function getStaticProps() {
