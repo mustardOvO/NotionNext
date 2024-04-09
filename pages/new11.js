94891212e74615f55ad10dfd45373958f47953a8
mustardOvO <mustardovo@gmail.com>
2024-04-09 18:34:16 +0800

    1

--- pages/new11.js
+++ pages/new11.js
@@ -9,16 +9,15 @@ import { siteConfig } from '@/lib/config'
  * @returns
  */
 const new1 = props => {
-  // 根据页面路径加载不同Layout文件
-  const Layout = getLayoutByTheme({ theme: siteConfig('THEME'), router: useRouter() })
-//   return <Layout {...props} />
-return <div className="w-full h-20 bg-red-400">111</div>
+    // 根据页面路径加载不同Layout文件
+    //   const Layout = getLayoutByTheme({ theme: siteConfig('THEME'), router: useRouter() })
+    //   return <Layout {...props} />
+    return <div className="w-full h-20 bg-red-400">111</div>
 }
 
-export async function getStaticProps () {
+export async function getStaticProps() {
     const props = (await getGlobalData({ from: 'new11' })) || {}
     return { props }
-  }
-
+}
 
 export default new1
