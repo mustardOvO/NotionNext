0e48ab5954621913b85c5a3d4e7276cb3c101a57
mustardOvO <mustardovo@gmail.com>
2024-06-14 19:22:51 +0800

    1

--- themes/00mine/components/RiveItem.js
+++ themes/00mine/components/RiveItem.js
@@ -3,9 +3,9 @@ import { useRive, Layout, Fit, Alignment } from "@rive-app/react-canvas";
 export const RiveDemo = () => {
   const { RiveComponent } = useRive({
     // Load a local riv `clean_the_car.riv` or upload your own!
-    src: "rive/clean_the_car.riv",
+    src: "rive/new_file.riv",
     // Be sure to specify the correct state machine (or animation) name
-    stateMachines: "Motion",
+    stateMachines: "State Machine 1",
     // This is optional.Provides additional layout control.
     layout: new Layout({
       fit: Fit.FitWidth, // Change to: rive.Fit.Contain, or Cover
@@ -28,9 +28,9 @@ export const UrlDemo = () => {
 
 export default function RiveItem() {
   return (
-    <div className="flex w-48 h-48 p-4">
-      <RiveDemo />
-      <UrlDemo />
+    <div className=" w-full h-[400px] ">
+      <div className=" w-[600px] h-[400px] mx-auto"> <RiveDemo /></div>
+      {/* <UrlDemo /> */}
     </div>
   );
 }
